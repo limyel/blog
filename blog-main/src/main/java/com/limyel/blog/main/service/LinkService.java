42fa82cbe1310dd601dca360fa3337bedbddc88a
limyel <limyel@outlook.com>
2023-12-19 15:37:41 +0800

    Link

---
+++ blog-main/src/main/java/com/limyel/blog/main/service/LinkService.java
@@ -0,0 +1,11 @@
+package com.limyel.blog.main.service;
+
+import com.limyel.blog.main.vo.LinkVO;
+
+import java.util.List;
+
+public interface LinkService {
+
+    List<LinkVO> list();
+
+}
---
+++ blog-main/src/main/java/com/limyel/blog/main/vo/LinkVO.java
@@ -0,0 +1,16 @@
+package com.limyel.blog.main.vo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LinkVO {
+
+    private String name;
+
+    private String description;
+
+    private String url;
+
+}
