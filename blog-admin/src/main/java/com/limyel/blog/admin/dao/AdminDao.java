b5a462284358e07aea5911cbe19e377727e7e715
limyel <limyel@outlook.com>
2024-01-08 15:47:27 +0800

    test

--- blog-admin/src/main/java/com/limyel/blog/admin/dao/AdminDao.java
+++ blog-admin/src/main/java/com/limyel/blog/admin/dao/AdminDao.java
@@ -4,6 +4,9 @@ import com.limyel.blog.admin.entity.AdminEntity;
 import com.limyel.blog.mybatis.dao.BaseDao;
 import org.apache.ibatis.annotations.Mapper;
 
+/**
+ *
+ */
 @Mapper
 public interface AdminDao extends BaseDao<AdminEntity> {
 
