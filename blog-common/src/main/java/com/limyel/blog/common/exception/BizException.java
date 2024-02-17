a56b258078622718114ad41b7d006d76656a63a7
limyel <limyel@outlook.com>
2024-02-17 21:42:14 +0800

    common

---
+++ blog-common/src/main/java/com/limyel/blog/common/exception/BizException.java
@@ -0,0 +1,36 @@
+package com.limyel.blog.common.exception;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class BizException extends RuntimeException {
+
+    private Integer code;
+
+    private String message;
+
+    /**
+     * 空构造方法，避免反序列化问题
+     */
+    public BizException() {
+    }
+
+    public BizException(ErrorCode errorCode) {
+        this.code = errorCode.getCode();
+        this.message = errorCode.getMsg();
+    }
+
+    public BizException(Integer code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public BizException(String message, Throwable e) {
+        super(message, e);
+        this.code = GlobalErrorCode.INTERNAL_SERVER_ERROR.getCode();
+        this.message = message;
+    }
+
+}
